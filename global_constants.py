f9cd1f337232ee2b5ce34472299c27173646d8a3
aaronworkspace1-admin <afon5477@uni.sydney.edu.au>
2025-03-27 10:45:42 +1100

    Slider added

--- global_constants.py
+++ global_constants.py
@@ -4,7 +4,8 @@ import random
 class constants():
     ##------VARIABLES------
     screenSize = [640,720]
-
+    humidityLevel = 55
+    vocLevel = 0
     ##------COLOURS------
     red = (255,0,0)
     blue = (0, 0, 255)
--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 import pygame
 import random
-
+import pygame_widgets
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
 from global_constants import constants
 
 
@@ -8,12 +10,18 @@ pygame.init()
 screen = pygame.display.set_mode(constants.screenSize)
 running = True
 screen.fill(constants.blue)
-
+slider = Slider(screen, 50, 100, 500, 40, min=0, max=99, step=1)
+output = TextBox(screen, 475, 200, 50, 50, fontSize=30)
 
 while running:
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             running = False
     pygame.display.flip()
-
+    output.setText(slider.getValue())
+    humidityLevel = slider.getValue()
+    print(humidityLevel)
+    pygame_widgets.update(events)
+    pygame.display.update()
 pygame.quit()
