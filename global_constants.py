708a46dad9cc3febfae0fc62eeb767aefe17edc4
phoebe_stage <phoebestage01@gmail.com>
2025-03-27 14:06:01 +1100

    added colours

--- global_constants.py
+++ global_constants.py
@@ -9,3 +9,9 @@ class constants():
     ##------COLOURS------
     red = (255,0,0)
     blue = (0, 0, 255)
+    light = (241, 233, 219)
+    mid_light = (93, 183, 222)
+    mid_dark = (48, 77, 109)
+    dark = (17,3,4)
+    accent = (250, 130, 75)
+    
