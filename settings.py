75308d450c254cbb4890337bfe2f73dab8d28047
aaronworkspace1-admin <afon5477@uni.sydney.edu.au>
2025-03-29 11:03:31 +1100

    Finished first design of settings and added a json file, not linked to python code yet

--- settings.py
+++ settings.py
@@ -5,14 +5,20 @@ from pygame_widgets.slider import Slider
 from pygame_widgets.textbox import TextBox
 from global_constants import constants
 
-
+# screenSize = [640,720]
 pygame.init()
 screen = pygame.display.set_mode(constants.screenSize)
 running = True
-screen.fill(constants.blue)
-slider = Slider(screen, 50, 100, 500, 40, min=0, max=99, step=1)
-output = TextBox(screen, 475, 200, 50, 50, fontSize=30)
+screen.fill(constants.mid_light)
+slider = Slider(screen, 50, 150, 500, 40, min=0, max=99, step=1, initial=0)
+output = TextBox(screen, 475, 50, 50, 50, fontSize=30)
+humidity_heading = TextBox(screen, 50, 50, 250, 50, fontSize = 30)
+humidity_heading.setText("Humidity Level (%)")
 
+poll_slider = Slider(screen, 50, 400, 500, 40, min=0, max=1000, step=1, initial=0)
+poll_output = TextBox(screen, 475, 300, 100, 50, fontSize=30)
+poll_heading = TextBox(screen, 50, 300, 300, 50, fontSize = 30)
+poll_heading.setText("Pollutant Level (ppb)")
 while running:
     events = pygame.event.get()
     for event in events:
@@ -20,8 +26,10 @@ while running:
             running = False
     pygame.display.flip()
     output.setText(slider.getValue())
+    poll_output.setText(poll_slider.getValue())
     constants.humidityLevel = slider.getValue()
-    print(constants.humidityLevel)
+    constants.vocLevel = poll_slider.getValue()
+    # print(constants.humidityLevel)
     pygame_widgets.update(events)
     pygame.display.update()
 pygame.quit()
